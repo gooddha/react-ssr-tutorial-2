505728a2a8f39a0d43cf6fbfeb06189987e51d38
gooddha <gooddha@ya.ru>
2020-03-01 18:51:33 +0300

    Find missprint

--- server/server.js
+++ server/server.js
@@ -12,13 +12,12 @@ const app = express();
 const PORT = 8000;
 
 app.use('^/$', (req, res, next) => {
-  fs.readFile(path.resolve('../build/index.html'), 'utf-8', (err, data) => {
+  fs.readFile(path.resolve('./build/index.html'), 'utf-8', (err, data) => {
     if (err) {
       console.log(err);
       return res.status(500).send("Some error happend")
     }
 
-    return res.send('123');
     return res.send(data.replace('<div id="root"></div>', `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`));
   })
 });
